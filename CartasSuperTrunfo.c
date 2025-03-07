369ee6ad6c83d5643361907fe4d46876ac2b19c9
karynne <202502824823@alunos.estacio.br>
2025-03-07 17:56:21 +0000

    Conclusão desafio mestre,

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,12 +7,14 @@ int main() {
     char Estado1[5], Estado2[5];
     char Codigo1[50], Codigo2[50];
     char Cidade1[50], Cidade2[50];
-    int Populacao1, Populacao2;
+    unsigned long int Populacao1, Populacao2;
     float Area1, Area2;
     float Pib1, Pib2;
     int PontosT1, PontosT2;
     float DensidadeP1, DensidadeP2;
     float PibPC1, PibPC2;
+    float superpoder1, superpoder2;
+    int resultado1, resultado2, resultado3, resultado4, resultado5, resultado6, resultado7;
 
     //A seguir dados da carta 1:
 
@@ -28,7 +30,7 @@ int main() {
     scanf("%s", &Cidade1);
 
     printf("Digite o número de habitantes da cidade:\n");
-    scanf("%d", &Populacao1);
+    scanf("%ld", &Populacao1);
 
     printf("Digite a área da cidade:\n");
     scanf("%f", &Area1);
@@ -42,6 +44,8 @@ int main() {
     DensidadeP1 = Populacao1 / Area1;
 
     PibPC1 = Pib1 / Populacao1;
+
+    superpoder1 = Populacao1 + Area1 + Pib1 + PontosT1 + PibPC1 + 1/DensidadeP1;
 
     printf("\n");
 
@@ -59,7 +63,7 @@ int main() {
     scanf("%s", &Cidade2);
 
     printf("digite o número de habitantes da cidade:\n");
-    scanf("%d", &Populacao2);
+    scanf("%ld", &Populacao2);
 
     printf("digite a área da cidade:\n");
     scanf("%f", &Area2);
@@ -74,6 +78,8 @@ int main() {
 
     PibPC2 = Pib2 / Populacao2;
 
+    superpoder2 = Populacao2 + Area2 + Pib2 + PontosT2 + PibPC2 + 1/DensidadeP2;
+
     printf("\n");
 
     //Exibição carta 1:
@@ -82,12 +88,13 @@ int main() {
     printf("Estado: %s\n", Estado1);
     printf("Codigo: %s\n", Codigo1);
     printf("Nome da cidade: %s\n", Cidade1);
-    printf("Populção: %d de habitantes\n", Populacao1);
+    printf("Populção: %ld de habitantes\n", Populacao1);
     printf("Àrea: %.1f km²\n", Area1);
-    printf("PIB: %.3f reais\n", Pib1);
+    printf("PIB: %.2f reais\n", Pib1);
     printf("Pontos turísticos: %d\n", PontosT1);
-    printf("Densidade populacional: %.3f hab/km²\n", DensidadeP1);
-    printf("PIB per capita: %.3f reais\n", PibPC1);
+    printf("Densidade populacional: %.1f hab/km²\n", DensidadeP1);
+    printf("PIB per capita: %.2f reais\n", PibPC1);
+    printf("Superpoder: %.1f\n", superpoder1);
     
     printf("\n");
     //Exibição carta 2:
@@ -96,12 +103,39 @@ int main() {
     printf("Estado: %s\n", Estado2);
     printf("Codigo: %s\n", Codigo2);
     printf("Nome da cidade: %s\n", Cidade2);
-    printf("Populção: %d de habitantes\n", Populacao2);
+    printf("População: %ld de habitantes\n", Populacao2);
     printf("Àrea: %.1f km²\n", Area2);
-    printf("PIB: %.3f reais\n", Pib2);
+    printf("PIB: %.2f reais\n", Pib2);
     printf("Pontos turísticos: %d\n", PontosT2);
-    printf("Densidade populacional: %.3f Hab/km²\n", DensidadeP2);
-    printf("Pib per capita: %.3f reais\n", PibPC2);
+    printf("Densidade populacional: %.1f Hab/km²\n", DensidadeP2);
+    printf("Pib per capita: %.2f reais\n", PibPC2);
+    printf("Superpoder: %.1f\n", superpoder2);
+
+    printf("\n");
+    //Comparação:
+
+    resultado1 = Populacao1 > Populacao2;
+    resultado2 = Area1 > Area2;
+    resultado3 = Pib1 > Pib2;
+    resultado4 = PontosT1 > PontosT2;
+    resultado5 = DensidadeP1 < DensidadeP2;
+    resultado6 = PibPC1 > PibPC2;
+    resultado7 = superpoder1 > superpoder2;
+
+    printf("\n");
+
+    //densidade populacional, a carta com o menor valor vence; 
+    //os demais atributos (incluindo Super Poder), a carta com o maior valor vence.
+
+    printf("Comparação de cartas:\n");
+    printf("População: Carta 1 venceu (%ld)\n", resultado1);
+    printf("Àrea: Carta 1 Venceu (%d)\n", resultado2);
+    printf("Pib: Carta 1 Venceu (%d)\n", resultado3);
+    printf("Pontos turisticos: Carta 1 Venceu (%d)\n", resultado4);
+    printf("densidade populacional: Carta 1 Venceu (%d)\n",resultado5);
+    printf("Pib per capita: Carta 1 Venceu (%d)\n", resultado6);
+    printf("Superpoder: Carta 1 venceu (%d)\n", resultado7);
+
 
 
     return 0;
